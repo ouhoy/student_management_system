1b8a5d6e83ca8ec96f371d1d86456ba54bbdbac3
ouhoy <kimkinon@gmail.com>
2022-12-31 17:13:03 +0100

    Add project's main functions

--- main.py
+++ main.py
@@ -12,22 +12,43 @@ def name_validation(prompt_string):
     return name
 
 
+def select_again(prompt_string):
+    selection = input(f"{prompt_string} If yes type Y otherwise hit enter to continue: ").strip().lower()
+    if selection == "y":
+        return True
+    return False
+
+
 students = []
 
-user_choice = input("What's your choice: ").strip().lower()
 
-if user_choice == "add":
-    student_name = name_validation("Please enter the name of the student: ")
-    students.append(student_name)
-if user_choice == "remove":
-    student_name = name_validation("Please enter the name of the student: ")
-    students.remove(student_name)
-if user_choice == "remove":
-    student_name = name_validation("Please enter the name of the student: ")
-    students.remove(student_name)
-if user_choice == "search":
-    student_name = name_validation("Please enter the name of the student: ")
-    try:
-        students.index(student_name)
-    except:
-        print("This student does not exist")
+def add_student():
+    name = name_validation("Enter student name")
+    if name in students:
+        print(f"The student's name: {name} you have entered already is in the record ")
+    return name
+
+
+def view_students():
+    print(f"Total students is: {len(students)}")
+    for index, student in enumerate(students):
+        print(f"{index + 1} - {student}")
+
+
+def search_student():
+    name = name_validation("Enter student name")
+    if name in students:
+        print(f"The student {name} is in the record")
+    else:
+        print(f"The student {name} is not in the record ")
+        if select_again("Would you like to add the student to the record? "):
+            add_student()
+
+
+def remove_student():
+    name = name_validation("Enter student name to remove: ")
+    if name in students:
+        return name
+    else:
+        print(f"The student {name} is not in the record ")
+        return False
